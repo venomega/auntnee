891b74d2ebbc9fa5a65bdd70c9443d8f76ad7248
venomega <guilleps92@nauta.cu>
2021-01-15 22:24:40 -0500

    added url

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
     packages=['auntnee', ],
     requires=["pyotp", "curses", ],
     licence='GPL-3',
+    url="https://github.com/venomega/auntnee",
+    download_url="https://github.com/venomega/auntnee/archive/0.1.tar.gz",
     description="This is an TOTP Authenticator CLI for the terminal",
     long_description=open("README.md").read(),
 
