84e390d26c032978373d4c3bb86bba2a2e688bdf
venomega <guilleps92@nauta.cu>
2021-07-25 11:40:12 -0400

    added option for create empty template in case a path on first program run does not exist or is empty

--- auntnee/err.py
+++ auntnee/err.py
@@ -1,13 +1,17 @@
 import sys
 import json
 import os
-
+import jfile
 
 def not_path():
     print("Failed to solve json file")
     print("Please enter the route to json file unencrypted", end="")
     path = input(": ")
-    #d = json.load(open(f"{os.environ['HOME']}/.auntnee.conf"))
-    #d['path'] = path
     d = {'path': path}
     json.dump(d, open(f"{os.environ['HOME']}/.auntnee.conf", 'w'))
+    try:
+        json.load(open(path))
+    except:
+        print ("File seems to not be an Aegis json, do you want to create an empty template [Y/n]: ",end="")
+        if not "n" in input().lower():
+            jfile.empty(path)
