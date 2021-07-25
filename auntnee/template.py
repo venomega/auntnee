d2052c4105e0691bcd4123562740197de9a79d9f
venomega <guilleps92@nauta.cu>
2021-07-25 11:38:47 -0400

    added json empty template

--- auntnee/template.py
+++ auntnee/template.py
@@ -1,3 +1,14 @@
 camp = {'type': 'totp', 'uuid': '', 'name': '', 'issuer': '', 'icon': None,
-         'info': {'secret': '', 'algo': 'SHA1', 'digits': 6, 'period': 30}}
+        'info': {'secret': '', 'algo': 'SHA1', 'digits': 6, 'period': 30}}
 
+empty = {
+    "version": 1,
+    "header": {
+        "slots": null,
+        "params": null
+    },
+    "db": {
+        "version": 1,
+        "entries": []
+    }
+}
