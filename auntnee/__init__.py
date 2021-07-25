8496d439a2e50d97b5b4070e2389fe4d2b852d98
venomega <guilleps92@nauta.cu>
2021-07-24 21:47:13 -0400

    added json file check on startup

--- auntnee/__init__.py
+++ auntnee/__init__.py
@@ -3,7 +3,7 @@ for now do nothing
 """
 import sys
 import os
-
+import json
 
 sys.path.append(__file__[:-11])
 
@@ -14,3 +14,11 @@ except:
     import err
     err.not_path()
     exit(0)
+
+try:
+    open(json.load(open(f"{os.environ['HOME']}/.auntnee.conf"))['path']).close()
+except:
+    print ("Can not access json file, Has the json file been moved/removed?")
+    err.not_path()
+    exit(0)
+
