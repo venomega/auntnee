4066b1998d53c5387e414e2344fd496eba1759cd
venomega <guilleps92@nauta.cu>
2021-07-31 15:37:59 -0400

    added delete help

--- auntnee/help.py
+++ auntnee/help.py
@@ -1,2 +1,2 @@
 def help():
-    print ("Usage:\n -h | --help\tPrint this help\n -a | --add\tAdd a new secrect to de db\n -c | --change\tchange path of db file\n\nExample:\npython3 -m auntnee -a mywebpage.com username KEY_TOTP\npython3 -m auntnee -c '/path/to/file'\n")
+    print ("Usage:\n -h | --help\tPrint this help\n -a | --add\tAdd a new secrect to de db\n -c | --change\tchange path of db file\n -d | --del\tDelete an entry by issuer (Example: test.com)\n\nExample:\npython3 -m auntnee -a mywebpage.com username KEY_TOTP\npython3 -m auntnee -c '/path/to/file'\npython3 -m auntnee -d mywebpage.com\n")
