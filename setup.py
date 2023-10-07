f9551f93c15fd461b315100eb2e6425d17db5587
venomega <guilleps92@gmail.com>
2023-10-07 19:44:05 -0400

    changed typo in license

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.2',
     packages=['auntnee', ],
     requires=["pyotp", "curses", ],
-    licence='GPL-3',
+    license='GPL-3',
     url="https://github.com/venomega/auntnee",
     download_url="https://github.com/venomega/auntnee/archive/0.2.tar.gz",
     description="This is an TOTP Authenticator CLI for the terminal"
